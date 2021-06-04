3e45912830aad50355cdb936ba88427b9c5d1f52
Guilherme Rossato <guilhermrossato@gmail.com>
2021-06-04 09:46:17 -0300

    Simplified compile windows loop

--- compile-windows-loop.bat
+++ compile-windows-loop.bat
@@ -11,23 +11,13 @@ if not exist "%ENVSCRIPT%" (
 WHERE cl
 IF %ERRORLEVEL% NEQ 0 (
 	echo Error: Could not find 'cl.exe', the Microsoft C/C++ Optimizing Compiler used by this script to compile
-	echo Ensure it is installed and available at path
+	echo Ensure it is installed and available at the path enviroment variable
 	pause
 	exit
 )
-WHERE timeit
-IF %ERRORLEVEL% NEQ 0 (
-	SET USETIMEIT=0
-) else (
-	SET USETIMEIT=1
-)
 :start
 title Compiling and Executing
-IF %USETIMEIT% NEQ 0 (
-	cl /nologo /DWIN32 /D_WINDOWS /Ob0 /O2 /I./portaudio/include /showIncludes main.c ./portaudio/src/os/win/pa_win_util.c ./portaudio/src/os/win/pa_win_hostapis.c ./portaudio/src/os/win/pa_win_waveformat.c ./portaudio/src/os/win/pa_win_wdmks_utils.c ./portaudio/src/os/win/pa_win_coinitialize.c ./portaudio/src/common/pa_front.c && echo. && main.exe 1
-) else (
-	cl /nologo /DWIN32 /D_WINDOWS /Ob0 /O2 /I./portaudio/include /showIncludes main.c ./portaudio/src/os/win/pa_win_util.c && echo. && main.exe 1
-)
+cl /nologo /DWIN32 /D_WINDOWS /Ob0 /O2 /I./portaudio/include /showIncludes main.c ./portaudio/src/os/win/pa_win_util.c ./portaudio/src/os/win/pa_win_hostapis.c ./portaudio/src/os/win/pa_win_waveformat.c ./portaudio/src/os/win/pa_win_wdmks_utils.c ./portaudio/src/os/win/pa_win_coinitialize.c ./portaudio/src/common/pa_front.c && echo. && main.exe 1
 title Waiting for key to recompile
 echo.
 pause
